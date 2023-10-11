f2bb00012b3450df03caffa2f18ed67fbd90e690
ledwards <ledwards@cbs.mpg.de>
2023-10-11 13:39:46 +0200

    Modularise reading DICOM header

--- readBvecsFromDicom.m
+++ readBvecsFromDicom.m
@@ -32,37 +32,12 @@ dictFile=fullfile(fileparts(mfilename('fullpath')),'dicom-dict-siemens.txt');
 files=dir(infolder);
 files=files(~[files.isdir]); % remove directories
 
-oldDictFile=dicomdict("get");
-try % run in try catch so we still reset the dicom dictionary back to "oldDictFile" even if the operations fail
-    dicomdict("set",dictFile);
-
-    nRep=length(files);
-    bNominal=zeros(1,nRep);
-    t=zeros(1,nRep);
-    B=zeros(6,nRep);
-    vNominal=zeros(3,nRep);
-    for n = 1:nRep
-        % Read dicom fields using Matlab objects. Faster than using dicominfo; see here: 
-        % https://de.mathworks.com/matlabcentral/answers/78315-can-i-read-a-single-field-using-dicominfo#answer_452571
-        di=images.internal.dicom.DICOMFile(fullfile(files(n).folder,files(n).name));
-        t(n) = di.getAttributeByName('AcquisitionNumber');
-        % Use "typecast" so still works when info on DICOM header types was lost and Matlab returns uint8 vector
-        B_tmp=typecast(di.getAttributeByName('B_matrix'),'double');
-        if ~isempty(B_tmp) % empty for b=0 acquisitions
-            % Convert DICOM "xx xy xz yy yz zz" to TWIX "xx yy zz xy xz yz"
-            B(:,n)=B_tmp([1,4,6,2,3,5]);
-
-            % Encode gradient polarity in B-matrix like in TWIX case
-            vNominal(:,n)=typecast(di.getAttributeByName('DiffusionGradientDirection'),'double');
-            B(1:3,n)=B(1:3,n).*sign(vNominal(:,n));
-        end
-        bNominal(n)=typecast(di.getAttributeByName('B_value'),'double');
-    end
-    dicomdict("set",oldDictFile); % make sure change to dictionary is not persistent
-
-catch ME
-    dicomdict("set",oldDictFile); % make sure change to dictionary is not persistent
-    rethrow(ME)
+nRep=length(files);
+bNominal=zeros(1,nRep);
+t=zeros(1,nRep);
+B=zeros(6,nRep);
+for n = 1:nRep
+    [t(n),bNominal(n),B(:,n)] = read0019fields(fullfile(files(n).folder,files(n).name),dictFile);
 end
 
 % Make sure that the output will match the acquisition order
@@ -79,5 +54,44 @@ saveBval(bValues,outfolder,output_name);
 
 % Also output nominal b-values in case they are needed
 saveBval(bNominal,outfolder,[output_name,'_nominal']);
+
+end
+
+%% Local functions
+% Read diffusion encoding information from Siemens (0019,10xx) private DICOM fields
+function [t,b,B] = read0019fields(dicomFile,dictFile)
+
+oldDictFile=dicomdict("get");
+try % run in try catch so we still reset the dicom dictionary back to "oldDictFile" even if the operations fail
+    dicomdict("set",dictFile);
+
+    % Read dicom fields using Matlab objects. Faster than using dicominfo; see here:
+    % https://de.mathworks.com/matlabcentral/answers/78315-can-i-read-a-single-field-using-dicominfo#answer_452571
+    di=images.internal.dicom.DICOMFile(dicomFile);
+    t=di.getAttributeByName('AcquisitionNumber');
+
+    % Use "typecast" so still works when info on DICOM header types was lost and Matlab returns uint8 vector
+    b_tmp=typecast(di.getAttributeByName('B_value'),'double');
+    if isempty(b_tmp)
+        error('B_value missing in DICOM header.')
+    else
+        b=b_tmp;
+    end
+
+    B_tmp=typecast(di.getAttributeByName('B_matrix'),'double');
+    if ~isempty(B_tmp) % empty for b=0 acquisitions
+        % Convert DICOM "xx xy xz yy yz zz" to TWIX "xx yy zz xy xz yz"
+        B=B_tmp([1,4,6,2,3,5]);
+
+        % Encode gradient polarity in B-matrix like in TWIX case
+        v=typecast(di.getAttributeByName('DiffusionGradientDirection'),'double');
+        B(1:3)=B(1:3).*sign(v);
+    end
+    dicomdict("set",oldDictFile); % make sure change to dictionary is not persistent
+
+catch ME
+    dicomdict("set",oldDictFile); % make sure change to dictionary is not persistent
+    rethrow(ME)
+end
 
 end
